5448d943d9514fe96538a035f245f12eb5b07131
yurika sakae <yurika.sun@gmail.com>
2019-07-19 10:33:22 -0700

    displayed character names

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-//import charGrid from "./components/charGrid";
+import CharGrid from "./components/CharGrid";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,18 +11,10 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  useEffect(() => {
-    axios
-      .get(`https://swapi.co/api/people/`)
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => console.log("starwars API is currently down: ", error));
-  }, []);
-
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <CharGrid />
     </div>
   );
 };

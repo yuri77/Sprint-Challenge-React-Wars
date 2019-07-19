f515682ae75c6879b9a4f30b1e5a711e80fa7d91
yurika sakae <yurika.sun@gmail.com>
2019-07-19 10:03:18 -0700

    set up API to parse data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+//import charGrid from "./components/charGrid";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios
+      .get(`https://swapi.co/api/people/`)
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => console.log("starwars API is currently down: ", error));
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
     </div>
   );
-}
+};
 
 export default App;
